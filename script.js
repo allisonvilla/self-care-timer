ee80116087dc738d0a5b38a96cf6e9099302b084
allison <allison.villa@outlook.com>
2022-02-08 18:22:23 -0500

    made initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// PLANNED FEATURES: 
+// Timer until break
+// User is able to adjust timer length
+// When timer goes off, print a random self-care idea to spend your break (ex. snack, look outside to rest your eyes, etc.)
+
